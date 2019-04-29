252308fa510ec1784d5da5d7524d163af22cd7ed
yaolin1 <yaolin1@xiaomi.com>
2019-04-29 18:34:01 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const Router = require('koa-better-router');
+
+const router = Router().loadMethods();
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello World Koa!'
+  })
+});
+
+router.get('/foo',async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello World foo!'
+  });
+});
+
+module.exports = router;
+
